19a2b01ca193ffce9d985916a4eece31176b7957
Kamil Gilfanov <kamilgilfanov6@gmail.com>
2025-04-27 13:54:39 +0300

    fix user service

--- TelegramBot/Application/Services/UserService.cs
+++ TelegramBot/Application/Services/UserService.cs
@@ -1,6 +1,25 @@
+using Domain.Abstractions.Repositories;
+using Domain.Abstractions.Services;
+using Domain.Entities;
+using Domain.Models.Dto;
+using Domain.Utils;
+
 namespace Application.Services;
 
-public class UserService
+public class UserService(IUnitOfWork unitOfWork): IUserService
 {
-    
+    public async Task<Result<UserEntity>> RegisterAsync(RegisterUserDto registerUserDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result> LoginAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result> UnregisterAsync(long userId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- TelegramBot/Domain/Abstractions/Services/IUserService.cs
+++ TelegramBot/Domain/Abstractions/Services/IUserService.cs
@@ -1,11 +1,12 @@
 using Domain.Entities;
+using Domain.Models.Dto;
 using Domain.Utils;
 
 namespace Domain.Abstractions.Services;
 
 public interface IUserService
 {
-    Task<Result<UserEntity>> RegisterAsync(UserEntity user);
-    Task<Result> LoginAsync(long chatId);
-    Task<Result> UnregisterAsync(long chatId);
+    Task<Result<UserEntity>> RegisterAsync(RegisterUserDto registerUserDto);
+    Task<Result> LoginAsync(long userId);
+    Task<Result> UnregisterAsync(long userId);
 }
