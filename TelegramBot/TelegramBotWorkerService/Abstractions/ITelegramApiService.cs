7a8dddbeb9c274308730d5f5c491664b8b11d51f
MaratAbashev <maratabashev@yandex.ru>
2025-04-29 05:28:11 +0300

    added telegram api service

--- TelegramBot/TelegramBotWorkerService/Abstractions/ITelegramApiService.cs
+++ TelegramBot/TelegramBotWorkerService/Abstractions/ITelegramApiService.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Models.Dto;
+using Domain.Models.Dto.Admin;
 using Domain.Models.Dto.Bot;
 using Domain.Models.Dto.General;
 
@@ -7,10 +8,11 @@ namespace TelegramBotWorkerService.Abstractions;
 public interface ITelegramApiService
 {
     Task RegisterUser(string phoneNumber, long userId);
-    Task<List<CourseDto>> GetCourses();
-    Task<CourseDto?> GetCourse(Guid courseId);
-    Task<ModuleDto?> GetModule(Guid moduleId);
-    Task<LessonDto?> GetLesson(Guid lessonId);
-    Task<BotQuestionResponseDto> StartTest(Guid lessonId);
-    Task<BotQuestionResponseDto> SendAnswer(Guid questionId, string answer);
+    Task<List<CourseDto>?> GetCourses(long userId);
+    Task<CourseDto?> GetCourse(long userId, Guid courseId);
+    Task<ModuleDto?> GetModule(long userId, Guid moduleId);
+    Task<LessonDto?> GetLesson(long userId, Guid lessonId);
+    Task<List<string>?> GetLessonContent(long userId, Guid lessonId);
+    Task<BotQuestionResponseDto?> StartTest(long userId, Guid lessonId);
+    public Task<BotQuestionResponseDto?> SendAnswer(long userId, Guid answerId, Guid? questionId = null);
 }
